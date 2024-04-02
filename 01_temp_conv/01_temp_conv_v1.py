b801fba4b50ccb5141492ca9133a1cfd2042fad8
Your Name <fb23419@student.phs.school.nz>
2024-04-02 20:09:46 +1300

    added buttons

--- 01_temp_conv/01_temp_conv_v1.py
+++ 01_temp_conv/01_temp_conv_v1.py
@@ -15,6 +15,28 @@ class converter:
         self.temp_instructions = Label(self.temp_frame, text=instructions,wrap=250,width=40,justify="left")
         self.temp_instructions.grid(row=1)
 
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Aria", "14"))
+        self.temp_entry.grid(row=2,padx=10,pady=10)
+
+        error = "Please enter a number"
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9C0000")
+        self.temp_error.grid(row=3)
+
+        # button frame
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+        
+        # buttons
+        self.to_celsius_button = Button(self.button_frame, text="To Degrees C",bg="#990099",fg="#FFFFFF")
+        self.to_celsius_button.grid(row=0,column=0)
+
+        self.to_farenheit_button = Button(self.button_frame, text="To Degrees F",bg="#009900",fg="#FFFFFF")
+        self.to_farenheit_button.grid(row=0,column=1)
+
+
+
 
 
 
