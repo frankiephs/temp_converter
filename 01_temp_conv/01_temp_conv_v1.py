9f6235ea308ee2fbcb24a4c99fefe701ca5b629e
Your Name <fb23419@student.phs.school.nz>
2024-04-02 17:13:40 +1300

    new file

---
+++ 01_temp_conv/01_temp_conv_v1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class converter:
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("temperature converter")
+    converter()
+    root.mainloop()
